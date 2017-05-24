eff84c97a12e76f076108b2e1a23443b4dfca705
Nicholas Luo <nicholasluo@gmail.com>
2017-05-25 00:23:20 +0800

    Add Responsive canvas scaling using react-dimensions

--- src/components/PictureViewer/index.js
+++ src/components/PictureViewer/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Dimensions from 'react-dimensions'
 import { Image, Layer, Rect, Stage, Group } from 'react-konva'
 import { loadPictureRequest, loadPictureSuccess } from '../../actions'
 
@@ -28,23 +29,28 @@ const mergeProps = (stateProps, { dispatch }, ownProps) => {
   }
 }
 
-let PictureViewer = ({ image, onClick }) => {
-  let picture
-  if(image !== null) {
-    picture = (<Image image={image} />)
+class PV extends Component {
+  render() {
+    const { containerWidth, image, onClick } = this.props
+    const height = Math.max(700, containerWidth)
+    let picture
+    if(image !== null) {
+      picture = (<Image image={image} />)
+    }
+    return (
+      <div>
+        <Stage width={containerWidth} height={height}>
+          <Layer>
+            {picture}
+          </Layer>
+        </Stage>
+        <button className="btn btn-default" onClick={onClick}>Load Image</button>
+      </div>
+    )
   }
-  return (
-    <div>
-      <Stage width={700} height={700}>
-        <Layer>
-          {picture}
-        </Layer>
-      </Stage>
-      <button className="btn btn-default" onClick={onClick}>Load Image</button>
-    </div>
-  )
 }
 
+let PictureViewer = Dimensions()(PV)
 PictureViewer = connect(
   mapStateToProps,
   null,
